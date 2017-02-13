f75453ff3fd5965077aa4f5f16b6ef1456321280
hawkroc <yinpengroc@gmail.com>
2017-02-13 15:57:44 +1300

    'fix'

--- src/header/containers/NavbarCon.jsx
+++ src/header/containers/NavbarCon.jsx
@@ -1,36 +1,33 @@
 import React, { Component } from 'react';
-import { Glyphicon,Col,Nav,NavItem,Navbar,NavDropdown,MenuItem} from 'react-bootstrap';
+import { SearchButton,MenuButton} from 'react-bootstrap';
 import { connect } from 'react-redux';
 import AutoAffix from 'react-overlays/lib/AutoAffix';
 import {handleSelect} from '../actions/actions';
 import Searcher from './Searcher';
 const navbarInstance = (
-  <Navbar>
-    <Col xs={4} md={2}>
-    <Navbar.Header >
-      <Navbar.Brand>
-        <a href="#">新中友协Logo</a>
-      </Navbar.Brand>
-    </Navbar.Header>
-    </Col>
-    <Nav>
-      <NavItem eventKey={1} href="#">首页</NavItem>
-      <NavItem eventKey={2} href="#">游览中国</NavItem>
-      <NavItem eventKey={2} href="#">在中国教书</NavItem>
-      <NavItem eventKey={2} href="#">新闻活动</NavItem>
-    <NavItem eventKey={4} ><Searcher/></NavItem>
+ 
+<div className="header">
+    <div className="topbar">
+        <div className="topbar-left">
+        </div>
+        <div className="topbar-right">
+        </div>
+    </div>
 
-      <NavDropdown eventKey={3} title="more"  id="basic-nav-dropdown">
-        <MenuItem eventKey={3.1}>Action</MenuItem>
-        <MenuItem eventKey={3.2}>Another action</MenuItem>
-        <MenuItem eventKey={3.3}>Something else here</MenuItem>
-        <MenuItem divider />
-        <MenuItem eventKey={3.3}>Separated link</MenuItem>
-      </NavDropdown>
+    <div className="menubar">
+        <div className="logo">
+        </div>
+        <div className="main-menu">
+        </div>
+        <div className="menu-buttons">
+          
+        </div>
+    </div>
 
-      
-    </Nav>
-  </Navbar>
+    <div className="dropmenu">
+    </div>
+</div>
+
 );
 
 class NavbarCon extends Component {
